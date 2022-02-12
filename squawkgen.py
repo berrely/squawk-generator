f7209caedfb9fbba02e90e426cce081133eb7d09
Berrely <kskordestani@gmail.com>
2022-02-12 15:59:12 +0000

    add check for duplicates

--- squawkgen.py
+++ squawkgen.py
@@ -1,5 +1,11 @@
 import random
 input("Press enter for a squawk. ")
+genSquawks=[]
+sequence="01234567"
 while True:
-    sequence="01234567"
-    input("".join(random.choices(sequence, k = 4)))
+    squawk="".join(random.choices(sequence, k = 4))
+    if squawk in genSquawks:
+        continue
+    else:
+        genSquawks.append(squawk)
+        input(squawk)
