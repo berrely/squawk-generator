e69f0c2939f485a0e91064c2fa29dc63f88eecdf
Berrely <20139168+berrely@users.noreply.github.com>
2022-05-05 11:44:02 +0100

    add check for emergency squawks

--- squawkgen.py
+++ squawkgen.py
@@ -1,10 +1,11 @@
 import random
 input("Press enter for a squawk. ")
 genSquawks=[]
+emergency=[7500,7600,7700]
 sequence="01234567"
 while True:
     squawk="".join(random.choices(sequence, k = 4))
-    if squawk in genSquawks:
+    if squawk in genSquawks or squawk in emergency:
         continue
     else:
         genSquawks.append(squawk)
